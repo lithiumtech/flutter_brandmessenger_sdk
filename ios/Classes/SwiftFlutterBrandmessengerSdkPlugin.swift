de1e2bc41b905f96749e1194df49ae70d97b089c
Pankaj Menariya <pankaj.menariya@trilogy.com>
2025-08-23 15:50:44 +0530

    dummy change to print log message for plugin initialised (#9)
    * Doc: release runbook added
    
    * updated cosmetic change

--- ios/Classes/SwiftFlutterBrandmessengerSdkPlugin.swift
+++ ios/Classes/SwiftFlutterBrandmessengerSdkPlugin.swift
@@ -199,7 +199,7 @@ public class SwiftFlutterBrandmessengerSdkPlugin: NSObject, FlutterPlugin, KBMCo
                 }
             }
         } else {
-            result("iOS " + UIDevice.current.systemVersion)
+            result("iOS version " + UIDevice.current.systemVersion)
         }
     }
     
